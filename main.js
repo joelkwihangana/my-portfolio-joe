7f2f39ba6e76d92992bd9c262c29e1f0cd25fd81
joelkwihangana <joelkwijoe@gmail.com>
2023-08-10 06:17:54 +0200

    fixing sections errors and animations

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Read more accordion functionality
+  const readMoreButtons = document.querySelectorAll(".read-more");
+  readMoreButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+      content.style.display =
+        content.style.display === "block" ? "none" : "block";
+      this.textContent =
+        content.style.display === "block" ? "Read less" : "Read more";
+    });
+  });
+});
